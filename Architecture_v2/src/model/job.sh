a9f078fef0b0e839ae4c87724b1ce0228bd2c853
Greg <ghunkins@users.noreply.github.com>
2017-12-09 11:28:22 -0500

    job.sh

---
+++ Architecture_v2/src/model/job.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -p gpu
+#SBATCH -t 0-08:00:00
+#SBATCH --job-name=pix
+#SBATCH --output=output_pix_%j.txt
+#SBATCH -e error_pix_%j.txt
+#SBATCH --gres=gpu:2
+
+source activate new_pix
+python main.py --backend tensorflow 64 64
