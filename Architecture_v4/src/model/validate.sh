a29be1effb026b8a86e47e16da31c03b35381825
Greg <ghunkins@users.noreply.github.com>
2017-12-21 10:31:07 -0500

    change log names

--- Architecture_v4/src/model/validate.sh
+++ Architecture_v4/src/model/validate.sh
@@ -3,8 +3,8 @@
 #SBATCH -t 0-08:00:00
 #SBATCH --mem 10GB
 #SBATCH --job-name=pix_val
-#SBATCH --output=validation_%j.txt
-#SBATCH -e validation_%j.txt
+#SBATCH --output=o_validation_%j.txt
+#SBATCH -e e_validation_%j.txt
 #SBATCH --gres=gpu:2
 
 source activate new_pix
